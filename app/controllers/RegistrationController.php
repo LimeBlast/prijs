d87a1ab424a703970a9f39a969cca8544c000a59
Daniel Hollands <daniel@limeblast.co.uk>
2014-06-23 18:02:35 +0100

    Lots of initial set-up.

---
+++ app/controllers/RegistrationController.php
@@ -0,0 +1,86 @@
+<?php
+
+class RegistrationController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 * GET /registration
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 * GET /registration/create
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return View::make('registration.create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /registration
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 * GET /registration/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 * GET /registration/{id}/edit
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 * PUT /registration/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 * DELETE /registration/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
---
+++ app/views/registration/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+
+	<div class="starter-template">
+		<h1>Register</h1>
+	</div>
+
+@stop
